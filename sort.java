68af837e6e18dea636d4a9858d5eb6f29ab05429
Nadil-Dulran <it23161788@my.sliit.lk>
2025-12-01 17:44:40 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Main{
+    public static void main(String [] args){
+        Integer[] arr1={2,6,3,4,9,2,7,4,};
+        Arrays.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+    }
+}
