bf377151536349b35428a53b0e6bc1856cd1583e
Nadil-Dulran <it23161788@my.sliit.lk>
2025-12-03 23:38:07 +0530

    Create list

---
+++ java/list.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(6,7,8,3,2,5,8,9,12);
+        Integer sum=0;
+        
+    Collections.sort(numbers);
+    System.out.println("Sorted List: " +numbers);
+        
+    Collections.reverse(numbers);
+    System.out.println("Reversed List: " +numbers);
+    
+    Integer max= Collections.max(numbers);
+    System.out.println("Max element in the List: " +max);
+    
+    for(int i=0; i<numbers.size(); i++){
+        sum+=numbers.get(i);
+    }
+    System.out.println("Sum of the List: " +sum);
+
+
+    }
+    
+}
