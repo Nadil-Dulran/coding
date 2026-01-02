165bce0c1eecb8ed2e6669fa34136ebc0fb91b36
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-02 19:56:52 +0530

    Create string java programme

---
+++ java/string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void main(String[] args) {
+
+        String str1 = "madam";   // string
+        // String str2 = "nadil";
+        
+        
+        
+        
+        Boolean palindrome = false;
+        if (palindrome) {
+            System.out.println("Palindrome string");
+        } else {
+            System.out.println("Not a palindrome string");
+        }
+    }
+}
